4549b52ed39e550c92f2c026726f2f3440ca6a2d
petrochenko <oleksii_petrochenko@epam.com>
2024-12-04 20:46:08 +0200

    Added csrf middleare usage

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ require_once __DIR__ . '/src/Http/Router.php';
 require 'vendor/autoload.php';
 
 use Enums\UserRole;
+use Http\Core\Session\SessionManager;
 use Http\Middlewares\AuthenticationMiddleware;
+use Http\Middlewares\CSRFMiddleware;
 use Http\Router;
 
 $container = require __DIR__ . '/src/Core/services.php';
@@ -13,7 +15,10 @@ $container = require __DIR__ . '/src/Core/services.php';
 $router = new Router($container);
 
 $router->addRoute('GET', '/', \Http\Controllers\PersonController::class, 'index');
-$router->addRoute('POST', '/', \Http\Controllers\PersonController::class, 'store', [new AuthenticationMiddleware([UserRole::ROLE_ADMIN])]);
+$router->addRoute('POST', '/', \Http\Controllers\PersonController::class, 'store', [
+    new AuthenticationMiddleware([UserRole::ROLE_ADMIN]),
+    new CSRFMiddleware($container->get(SessionManager::class))
+]);
 
 
 $router->addRoute('GET', '/find', \Http\Controllers\PersonController::class, 'show', [new AuthenticationMiddleware([UserRole::ROLE_USER])]);
